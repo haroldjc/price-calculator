44a5bb4c3ec687d42dcac4afeff7333fbacca594
haroldjc <harold@blakzr.com>
2023-02-18 17:42:49 -0500

    Refactor props in Ingredient list components

--- src/components/product/IngredientItem.js
+++ src/components/product/IngredientItem.js
@@ -1,17 +1,17 @@
-const IngredientItem = ({ ingredient, supplies, handleAmountChange, handleDeleteIngredient }) => {
+const IngredientItem = props => {
 
-  const supply = supplies.find(supply => supply.id === ingredient.id)
+  const supply = props.supplies.find(supply => supply.id === props.ingredient.id)
 
   return (
     <div className='ingredient-item'>
       <span className='ingredient-item__name'>
         {supply.name}
       </span>
-      <input type='number' value={ingredient.amount} onChange={e => handleAmountChange(ingredient.id, e)} />
+      <input type='number' value={props.ingredient.amount} onChange={e => props.handleAmountChange(props.ingredient.id, e)} />
       <span className='ingredient-item__unit'>
         {supply.unit}
       </span>
-      <button className='ingredient-item__delete' onClick={() => handleDeleteIngredient(ingredient.id)}>x</button>
+      <button className='ingredient-item__delete' onClick={() => props.handleDeleteIngredient(props.ingredient.id)}>x</button>
     </div>
   );
 }
--- src/components/product/IngredientsList.js
+++ src/components/product/IngredientsList.js
@@ -1,20 +1,20 @@
 import './IngredientsList.css';
 import IngredientItem from './IngredientItem';
 
-const IngredientsList = ({ product, supplies, handleAmountChange, handleDeleteIngredient }) => {
+const IngredientsList = props => {
 
   return (
     <div className='ingredients-list'>
       {
-        product.ingredients.length === 0
+        props.product.ingredients.length === 0
           ? <p className='no-ingredients'>Usa el buscador de ingredientes inferior para agregarlos a este producto.</p>
-          : product.ingredients.map(ingredient =>
+          : props.product.ingredients.map(ingredient =>
             <IngredientItem
               key={ingredient.id}
               ingredient={ingredient}
-              supplies={supplies}
-              handleAmountChange={handleAmountChange}
-              handleDeleteIngredient={handleDeleteIngredient}
+              supplies={props.supplies}
+              handleAmountChange={props.handleAmountChange}
+              handleDeleteIngredient={props.handleDeleteIngredient}
             />)
       }
     </div>
